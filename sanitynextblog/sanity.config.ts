a7186fd5b5cf1ea424c9f5a59c888e05cfcce395
Manas Kshatriya <manaskshatriya01@gmail.com>
2022-12-24 18:22:04 +0530

    pray

--- sanitynextblog/sanity.config.ts
+++ sanitynextblog/sanity.config.ts
@@ -1,18 +1,19 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+// import {defineConfig} from 'sanity'
+// import {deskTool} from 'sanity/desk'
+// import {visionTool} from '@sanity/vision'
+// import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'sanity_next_blog',
+// export default defineConfig({
+//   name: 'default',
+//   title: 'sanity_next_blog',
 
-  projectId: 'w84p5k3o',
-  dataset: 'production',
+//   projectId: 'w84p5k3o',
+//   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+//   plugins: [deskTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
-})
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
+export{}
